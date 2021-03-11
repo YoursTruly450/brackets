866744085c5642a8a716cdf8ec88952c2a4885f7
YoursTruly450 <yourstruly.bill@gmail.com>
2021-03-11 17:37:17 +0400

    feat: fix check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let n = arr.length;
+  while (n > 0) {
+    let newArr = arr.join('');
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let buffArr = newArr;
+      let strSplit = bracketsConfig[i][0] + bracketsConfig[i][1];
+      newArr = buffArr.split(strSplit).join('');
+    }
+    if (newArr.length === arr.length) {
+      break;
+    } else {
+      arr = newArr.split('');
+      n = arr.length;
+    }
+  }
+  return n === 0 ? true : false;
 }
